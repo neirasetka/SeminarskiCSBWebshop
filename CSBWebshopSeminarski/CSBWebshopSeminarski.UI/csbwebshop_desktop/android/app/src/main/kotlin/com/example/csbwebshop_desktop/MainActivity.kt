5a42811a7d993584036747ee9d0477e37b45d527
Neira Setka <neira.setka@edu.fit.ba>
2025-09-08 12:31:57 +0200

    remove unused folders

--- CSBWebshopSeminarski/CSBWebshopSeminarski.UI/csbwebshop_desktop/android/app/src/main/kotlin/com/example/csbwebshop_desktop/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.csbwebshop_desktop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- CSBWebshopSeminarski/CSBWebshopSeminarski.UI/csbwebshop_mobile/tool/run_staging.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-APP_DIR="${SCRIPT_DIR}/.."
-cd "$APP_DIR"
-/workspace/flutter/flutter/bin/flutter run --dart-define=APP_ENV=staging "$@"
-
