86c59086ce6f62b81c1ca4e6b93721442a76beb3
neirasetka <neira.setka@edu.fit.ba>
2026-03-03 14:30:00 +0100

    stripe

---
+++ CSBWebshopSeminarski/CBSWebshopSeminarski.Model/Requests/CreateCheckoutSessionResponse.cs
@@ -0,0 +1,8 @@
+namespace CBSWebshopSeminarski.Model.Requests
+{
+    public class CreateCheckoutSessionResponse
+    {
+        public string Url { get; set; } = string.Empty;
+        public string SessionId { get; set; } = string.Empty;
+    }
+}
