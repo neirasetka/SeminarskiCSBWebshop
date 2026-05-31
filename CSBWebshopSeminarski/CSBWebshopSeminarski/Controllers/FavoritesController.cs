3fa8ba0d32e27555de141ef895521c8ae0f45749
Neira Šetka <neira.setka@edu.fit.ba>
2026-05-31 12:32:01 +0200

    fix favorites bug

--- CSBWebshopSeminarski/CSBWebshopSeminarski/Controllers/FavoritesController.cs
+++ CSBWebshopSeminarski/CSBWebshopSeminarski/Controllers/FavoritesController.cs
@@ -1,6 +1,7 @@
 ﻿using CBSWebshopSeminarski.Model.Models;
 using CBSWebshopSeminarski.Model.Requests;
 using CBSWebshopSeminarski.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSBWebshopSeminarski.Controllers
@@ -11,6 +12,20 @@ namespace CSBWebshopSeminarski.Controllers
     {
         public FavoritesController(ICRUDService<Favorite, FavoriteSearchRequest, FavoriteUpsertRequest, FavoriteUpsertRequest> service) : base(service)
         {
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public override async Task<PagedResult<Favorite>> Get([FromQuery] FavoriteSearchRequest search)
+        {
+            return await base.Get(search);
+        }
+
+        [HttpGet("{ID:int}")]
+        [Authorize(Roles = "Admin")]
+        public override async Task<Favorite> GetById(int ID)
+        {
+            return await base.GetById(ID);
         }
     }
 }
